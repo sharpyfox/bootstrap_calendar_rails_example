08a5c7ee41e3f61ec3912d818d39447b168011ba
Nikita Vasiliev <sharpyfox@gmail.com>
2013-06-15 01:44:52 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalendarSample::Application.config.secret_token = '264343389dcbdc9f74bbfdd639f860426152ef4e11e3358d7d58b8b9b077ea0d8eecfc858d7df26c416b7fbb5cdb6fea3a0a8114f84786d8c2d49b3c48818ad7'
